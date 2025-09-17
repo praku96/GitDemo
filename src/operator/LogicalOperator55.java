d843dbf0dc255ac4c3710c21e87c76fdcce56cea
praku96 <prathameshkul96@gmail.com>
2025-09-17 16:32:02 +0530

    QAQA

---
+++ src/operator/LogicalOperator55.java
@@ -0,0 +1,26 @@
+package operator;
+
+public class LogicalOperator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		int a=10;
+		int b=5;
+		int c=20;
+		
+		
+		
+		//logical opertor &&= if 1st condition is false, second condition will not  check
+		System.out.println(a<b && a<c);
+	
+	//Bitwise operator -it will check all conf=ditions
+		System.out.println(a<b & a<c);
+		
+	
+	
+	
+	}
+
+}
